15ce75c5301241597da95cade408e2626db54c42
Rebecca Xu <rebeccaxu@Rebeccas-MacBook-Pro-80.local>
2025-05-13 00:59:51 -0500

    testing with other photos

--- compare.py
+++ compare.py
@@ -4,32 +4,21 @@
   
 from __future__ import print_function, unicode_literals 
 from facepplib import FacePP, exceptions 
-  
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
    
 # define face comparing function 
-def face_comparing(app, Image1, Image2): 
-      
-    print() 
-    print('-'*30) 
-    print('Comparing Photographs......') 
-    print('-'*30) 
-  
-   
+def face_compare(app, Image1, Image2): 
     cmp_ = app.compare.get(image_url1 = Image1, 
                            image_url2 = Image2) 
    
-    print('Photo1', '=', cmp_.image1) 
-    print('Photo2', '=', cmp_.image2) 
+    return cmp_.confidence
 
-    print('Confidence', '=', cmp_.confidence)
+def get_image_url(path):
+    base_url = 'https://github.com/Rebeccaxu0/cs312/blob/main/'
+    return base_url + path
    
-    # Comparing Photos 
-    if cmp_.confidence > 70: 
-        print('Both photographs are of same person......') 
-    else: 
-        print('Both photographs are of two different persons......') 
-  
-          
 # Driver Code  
 if __name__ == '__main__': 
    
@@ -37,16 +26,27 @@ if __name__ == '__main__':
     api_key ='xQLsTmMyqp1L2MIt7M3l0h-cQiy0Dwhl'
     api_secret ='TyBSGw8NBEP9Tbhv_JbQM18mIlorY6-D'
    
-    try: 
            
-        # call api 
-        app_ = FacePP(api_key = api_key,  
-                      api_secret = api_secret) 
-          
-        # Pair
-        image1 = 'https://ew.com/thmb/52qjEp3tdLZOHZG80S9GDlDRgoo=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Sabrina-Carpenter-080624-1-305ff7f67fba42569018822292f09f09.jpg'
-        image2 = 'https://media.glamour.com/photos/674de951e3207eff7c778ed4/master/w_2560%2Cc_limit/GettyImages-2177695508.jpg'
-        face_comparing(app_, image1, image2)         
-   
-    except exceptions.BaseFacePPError as e: 
-        print('Error:', e) 
+    # call api 
+    app_ = FacePP(api_key = api_key,  
+                    api_secret = api_secret) 
+    
+    synthetic_folder = 'Synthetic-Images'
+    training_folder = 'Training-Images'
+
+    synthetic_images = [f for f in os.listdir(synthetic_folder)]
+    training_images = [f for f in os.listdir(training_folder)]
+        
+    # Create an empty DataFrame to store results
+    similarity_matrix = pd.DataFrame(index=synthetic_images, columns=training_images)
+
+    # Loop through each pair
+    for synth_img in synthetic_images:
+        for train_img in training_images:
+            url1 = get_image_url(os.path.join(synthetic_folder, synth_img))
+            url2 = get_image_url(os.path.join(training_folder, train_img))
+            similarity_matrix.at[synth_img, train_img] = face_compare(app_, url1, url2)
+
+    
+    plt.figure()
+    plt.imshow(similarity_matrix)
